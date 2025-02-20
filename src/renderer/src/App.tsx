b942948ce9b1ac2dfb59333c57d81fe07c7bdf79
T Scott <contact@tascott.co.uk>
2025-02-20 12:47:11 +0000

    App setup

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,16 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <div className="text">digress</div>
+      <div className="actions">
+        <div className="action">
+          <a href="/home" target="_blank" rel="noreferrer">
+            Enter
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
