a48b41f7ab52f4ab83a56b0eb3abd6d3e6ac98a4
T Scott <contact@tascott.co.uk>
2025-03-04 17:17:56 +0000

    New modern boilerplate

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,90 @@
-import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
+import { resolve, normalize, dirname } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+
+import injectProcessEnvPlugin from 'rollup-plugin-inject-process-env'
+import tsconfigPathsPlugin from 'vite-tsconfig-paths'
+import reactPlugin from '@vitejs/plugin-react'
+
+import { settings } from './src/lib/electron-router-dom'
+import { main, resources } from './package.json'
+
+const [nodeModules, devFolder] = normalize(dirname(main)).split(/\/|\\/g)
+const devPath = [nodeModules, devFolder].join('/')
+
+const tsconfigPaths = tsconfigPathsPlugin({
+  projects: [resolve('tsconfig.json')],
+})
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
+
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve('src/main/index.ts'),
+        },
+
+        output: {
+          dir: resolve(devPath, 'main'),
+        },
+      },
     },
-    plugins: [react()]
-  }
+  },
+
+  preload: {
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
+
+    build: {
+      outDir: resolve(devPath, 'preload'),
+    },
+  },
+
+  renderer: {
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.platform': JSON.stringify(process.platform),
+    },
+
+    server: {
+      port: settings.port,
+    },
+
+    plugins: [
+      tsconfigPaths,
+      tailwindcss(),
+      reactPlugin(),
+
+      codeInspectorPlugin({
+        bundler: 'vite',
+        hotKeys: ['altKey'],
+        hideConsole: true,
+      }),
+    ],
+
+    publicDir: resolve(resources, 'public'),
+
+    build: {
+      outDir: resolve(devPath, 'renderer'),
+
+      rollupOptions: {
+        plugins: [
+          injectProcessEnvPlugin({
+            NODE_ENV: 'production',
+            platform: process.platform,
+          }),
+        ],
+
+        input: {
+          index: resolve('src/renderer/index.html'),
+        },
+
+        output: {
+          dir: resolve(devPath, 'renderer'),
+        },
+      },
+    },
+  },
 })
--- src/renderer/src/components/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = (): JSX.Element => {
-  return (
-    <div className="dashboard">
-      <h2>Dashboard</h2>
-    </div>
-  )
-}
-
-export default Dashboard
